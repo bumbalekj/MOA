c95dba27b01cb7570489f48a67fb4fa55ae5ace3
Bumbálek Jakub <L20220061@SSPTAJI.CZ>
2024-09-30 10:52:33 +0200

    fotky do slozky

--- service-worker.js
+++ service-worker.js
@@ -19,8 +19,8 @@ self.addEventListener('install', event => {
         '/index.html',
         '/style.css',
         '/app.js',
-        '/icon-192.png',
-        '/icon-512.png'
+        '/icons/icon-192.png',
+        '/icons/icon-512.png'
       ]);
     })
   );
